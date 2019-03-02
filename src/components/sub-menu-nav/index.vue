bf3946ea848827d743efa1d5b3dad39db6d937f7
chenxi <2277467367@qq.com>
2019-03-02 15:09:28 +0800

    改菜单icon颜色颜色试

--- src/components/sub-menu-nav/index.vue
+++ src/components/sub-menu-nav/index.vue
@@ -1,26 +1,28 @@
 <template>
-  <el-submenu
-    v-if="menuNav.children && menuNav.children.length >= 1"
-    :data-idx="menuNav.menuId + ''"
-    :index="menuNav.menuId + ''">
-    <template slot="title">
+  <div class="cx">
+    <el-submenu
+      v-if="menuNav.children && menuNav.children.length >= 1"
+      :data-idx="menuNav.menuId + ''"
+      :index="menuNav.menuId + ''">
+      <template slot="title" style="color: red">
+        <i :class="['site-sidebar__menu-icon', menuNav.icon ? menuNav.icon : 'icon-custom-shouye']"></i>
+        <span>{{ menuNav.name }}</span>
+      </template>
+      <sub-menu-nav
+        v-for="item in menuNav.children"
+        :key="item.menuId"
+        :menu-nav="item">
+      </sub-menu-nav>
+    </el-submenu>
+    <el-menu-item
+      v-else
+      :index="menuNav.menuId + ''"
+      :data-idx="menuNav.menuId + ''"
+      @click="gotoRouteHandle(menuNav.url)">
       <i :class="['site-sidebar__menu-icon', menuNav.icon ? menuNav.icon : 'icon-custom-shouye']"></i>
       <span>{{ menuNav.name }}</span>
-    </template>
-    <sub-menu-nav
-      v-for="item in menuNav.children"
-      :key="item.menuId"
-      :menu-nav="item">
-    </sub-menu-nav>
-  </el-submenu>
-  <el-menu-item
-    v-else
-    :index="menuNav.menuId + ''"
-    :data-idx="menuNav.menuId + ''"
-    @click="gotoRouteHandle(menuNav.url)">
-    <i :class="['site-sidebar__menu-icon', menuNav.icon ? menuNav.icon : 'icon-custom-shouye']"></i>
-    <span>{{ menuNav.name }}</span>
-  </el-menu-item>
+    </el-menu-item>
+  </div>
 </template>
 
 <script>
@@ -45,3 +47,10 @@ export default {
   }
 }
 </script>
+<style type="text/css" lang="scss" scoped>
+.cx{
+  & /deep/ .el-submenu__icon-arrow {
+    color: lawngreen;
+  }
+}
+</style>
