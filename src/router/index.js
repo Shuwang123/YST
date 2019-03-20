20e51a1eb7fb04345be4ce76e17e9550e143ef02
Shuwang123 <457388545@qq.com>
2019-03-20 10:40:13 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -35,7 +35,7 @@ var router = new Router({
         /* 门店管理End */
 
         /* 进销存模块：药品Begin */
-        {path: '/drugs/nameList', component: _import('drugs/index'), name: 'nameList', meta: {title: '药名列表', keepAlive: false}}
+        {path: '/drugs/nameList', component: _import('drugs/index'), name: 'nameList', meta: {title: '药名列表', keepAlive: false}},
         /* 进销存模块End */
         /* 柜台模块：挂号Begin */
         {path: '/register/index', component: _import('register/index'), name: 'register', meta: {title: '挂号', keepAlive: false}}
